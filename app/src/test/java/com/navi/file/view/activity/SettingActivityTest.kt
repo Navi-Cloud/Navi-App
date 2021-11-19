629eae577ac7f14d3067a146207db00929a61828
jiwoo <hjw6112@gmail.com>
2021-11-19 16:59:05 +0900

    SettingActivityTest

---
+++ app/src/test/java/com/navi/file/view/activity/SettingActivityTest.kt
@@ -0,0 +1,65 @@
+package com.navi.file.view.activity
+
+import android.content.Context
+import android.os.Build
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.core.app.launchActivity
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.navi.file.helper.ViewModelFactory
+import com.navi.file.hilt.ViewModelFactoryModule
+import com.navi.file.view.fragment.ViewModelTestHelper
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.HiltTestApplication
+import dagger.hilt.android.testing.UninstallModules
+import dagger.hilt.components.SingletonComponent
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.kotlin.mock
+import org.robolectric.annotation.Config
+import javax.inject.Singleton
+
+@UninstallModules(ViewModelFactoryModule::class)
+@HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
+@Config(sdk = [Build.VERSION_CODES.Q], manifest = Config.NONE, application = HiltTestApplication::class)
+class SettingActivityTest : ViewModelTestHelper(){
+    // Application Context
+    private lateinit var context: Context
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Module
+    @InstallIn(SingletonComponent::class)
+    inner class ViewModelFactoryTestModule {
+        @Provides
+        @Singleton
+        fun provideTestFactory(): ViewModelFactory {
+            return mock {
+            }
+        }
+    }
+
+    @Before
+    fun createInitialMock() {
+        context = ApplicationProvider.getApplicationContext()
+        hiltRule.inject()
+    }
+
+    @Test
+    fun is_settingActivity_is_alive(){
+        launchActivity<SettingActivity>().apply {
+            moveToState(Lifecycle.State.RESUMED)
+        }.also {
+            Assert.assertEquals(Lifecycle.State.RESUMED, it.state)
+        }
+    }
+}
